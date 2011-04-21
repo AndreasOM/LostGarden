f3b223760912935ee113f096fda9de82fa8f3e36
Andreas Neukoetter <andreas@omni-mad.com>
2011-04-21 15:57:22 +0200

    Added some junk. Increased atlas size.

--- Sinistar/2om-atlas.sh
+++ Sinistar/2om-atlas.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-omnitexture -p 2 -m RGBA8888 -o sinistar-01.otx -s 512 -c xz -x -d omni-mad-atlas \
+omnitexture -p 2 --border=1 -m RGBA8888 -o sinistar-01.otx -s 1024 -c xz -x -d omni-mad-atlas \
 	omni-mad/actgrid-?-??-??.png \
 	omni-mad/barrel-?-??-??.png  \
 	omni-mad/base-?-??-??.png \
@@ -18,7 +18,8 @@ omnitexture -p 2 -m RGBA8888 -o sinistar-01.otx -s 512 -c xz -x -d omni-mad-atla
 	omni-mad/exhaust-*-??-??.png \
 	omni-mad/explode-*-??-??.png \
 	omni-mad/guard-*-??-??.png \
-	omni-mad/hunter-*-??-??.png 
+	omni-mad/hunter-*-??-??.png \
+	omni-mad/junk-*-??-??.png 
 	
 omnitexture -p 2 -m RGB888 -o sinistar-interface-opaque.otx -s 512 -c xz -x -d omni-mad-atlas omni-mad/main-background-00-00.png
 omnitexture -p 2 -m RGBA8888 -o sinistar-interface-transparent.otx -s 512 -c xz -x -d omni-mad-atlas omni-mad/main-bar-??-00.png
