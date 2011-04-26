99ee451bb12bcd4bf58eee0c7aac15d7463fd11f
Andreas Neukoetter <andreas@omni-mad.com>
2011-04-26 18:11:52 +0200

    Lots of tweaks from last weekend.

---
+++ Sinistar/junk.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+
+rm tmp/*
+
+convert original/junk2.bmp -transparent '#000000' -crop 48x48+0+1     PNG32:omni-mad/junk-2-00-00.png
+convert original/junk2.bmp -transparent '#000000' -crop 48x48+72+1    PNG32:omni-mad/junk-2-10-00.png
+convert original/junk2.bmp -transparent '#000000' -crop 48x48+144+1   PNG32:omni-mad/junk-2-20-00.png
+convert original/junk2.bmp -transparent '#000000' -crop 48x48+0+73    PNG32:omni-mad/junk-2-01-00.png
+convert original/junk2.bmp -transparent '#000000' -crop 48x48+72+73   PNG32:omni-mad/junk-2-11-00.png
+convert original/junk2.bmp -transparent '#000000' -crop 48x48+144+73  PNG32:omni-mad/junk-2-21-00.png
+convert original/junk2.bmp -transparent '#000000' -crop 48x48+0+145   PNG32:omni-mad/junk-2-02-00.png
+convert original/junk2.bmp -transparent '#000000' -crop 48x48+72+145  PNG32:omni-mad/junk-2-12-00.png
+convert original/junk2.bmp -transparent '#000000' -crop 48x48+144+145 PNG32:omni-mad/junk-2-22-00.png
+
+convert original/junk3.bmp -transparent '#000000' -crop 48x48+0+0     PNG32:omni-mad/junk-3-00-00.png
+convert original/junk3.bmp -transparent '#000000' -crop 48x48+72+0    PNG32:omni-mad/junk-3-10-00.png
+convert original/junk3.bmp -transparent '#000000' -crop 48x48+144+0   PNG32:omni-mad/junk-3-20-00.png
+convert original/junk3.bmp -transparent '#000000' -crop 48x48+0+72    PNG32:omni-mad/junk-3-01-00.png
+convert original/junk3.bmp -transparent '#000000' -crop 48x48+72+72   PNG32:omni-mad/junk-3-11-00.png
+convert original/junk3.bmp -transparent '#000000' -crop 48x48+144+72  PNG32:omni-mad/junk-3-21-00.png
+convert original/junk3.bmp -transparent '#000000' -crop 48x48+0+144   PNG32:omni-mad/junk-3-02-00.png
+convert original/junk3.bmp -transparent '#000000' -crop 48x48+72+144  PNG32:omni-mad/junk-3-12-00.png
+convert original/junk3.bmp -transparent '#000000' -crop 48x48+144+144 PNG32:omni-mad/junk-3-22-00.png
+
+convert original/junk4.bmp -transparent '#000000' -crop 48x48+0+0     PNG32:omni-mad/junk-4-00-00.png
+convert original/junk4.bmp -transparent '#000000' -crop 48x48+72+0    PNG32:omni-mad/junk-4-10-00.png
+convert original/junk4.bmp -transparent '#000000' -crop 48x48+144+0   PNG32:omni-mad/junk-4-20-00.png
+convert original/junk4.bmp -transparent '#000000' -crop 48x48+0+72    PNG32:omni-mad/junk-4-01-00.png
+convert original/junk4.bmp -transparent '#000000' -crop 48x48+72+72   PNG32:omni-mad/junk-4-11-00.png
+convert original/junk4.bmp -transparent '#000000' -crop 48x48+144+72  PNG32:omni-mad/junk-4-21-00.png
+convert original/junk4.bmp -transparent '#000000' -crop 48x48+0+144   PNG32:omni-mad/junk-4-02-00.png
+convert original/junk4.bmp -transparent '#000000' -crop 48x48+72+144  PNG32:omni-mad/junk-4-12-00.png
+convert original/junk4.bmp -transparent '#000000' -crop 48x48+144+144 PNG32:omni-mad/junk-4-22-00.png
+
+convert original/junk5.bmp -transparent '#000000' -crop 48x48+0+0     PNG32:omni-mad/junk-5-00-00.png
+convert original/junk5.bmp -transparent '#000000' -crop 48x48+72+0    PNG32:omni-mad/junk-5-10-00.png
+convert original/junk5.bmp -transparent '#000000' -crop 48x48+144+0   PNG32:omni-mad/junk-5-20-00.png
+convert original/junk5.bmp -transparent '#000000' -crop 48x48+0+72    PNG32:omni-mad/junk-5-01-00.png
+convert original/junk5.bmp -transparent '#000000' -crop 48x48+72+72   PNG32:omni-mad/junk-5-11-00.png
+convert original/junk5.bmp -transparent '#000000' -crop 48x48+144+72  PNG32:omni-mad/junk-5-21-00.png
+convert original/junk5.bmp -transparent '#000000' -crop 48x48+0+144   PNG32:omni-mad/junk-5-02-00.png
+convert original/junk5.bmp -transparent '#000000' -crop 48x48+72+144  PNG32:omni-mad/junk-5-12-00.png
+convert original/junk5.bmp -transparent '#000000' -crop 48x48+144+144 PNG32:omni-mad/junk-5-22-00.png
+
+convert original/junk6.bmp -transparent '#000000' -crop 48x48+0+0     PNG32:omni-mad/junk-6-00-00.png
+convert original/junk6.bmp -transparent '#000000' -crop 48x48+72+0    PNG32:omni-mad/junk-6-10-00.png
+convert original/junk6.bmp -transparent '#000000' -crop 48x48+144+0   PNG32:omni-mad/junk-6-20-00.png
+convert original/junk6.bmp -transparent '#000000' -crop 48x48+0+72    PNG32:omni-mad/junk-6-01-00.png
+convert original/junk6.bmp -transparent '#000000' -crop 48x48+72+72   PNG32:omni-mad/junk-6-11-00.png
+convert original/junk6.bmp -transparent '#000000' -crop 48x48+144+72  PNG32:omni-mad/junk-6-21-00.png
+convert original/junk6.bmp -transparent '#000000' -crop 48x48+0+144   PNG32:omni-mad/junk-6-02-00.png
+convert original/junk6.bmp -transparent '#000000' -crop 48x48+72+144  PNG32:omni-mad/junk-6-12-00.png
+convert original/junk6.bmp -transparent '#000000' -crop 48x48+144+144 PNG32:omni-mad/junk-6-22-00.png
+
